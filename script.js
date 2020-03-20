5d933537337c7850e1f47dadccc415e0e460b410
JidMenosta <jidmenosta@gmail.com>
2020-03-20 17:03:13 -0400

    added a table to HTML

--- script.js
+++ script.js
@@ -2,25 +2,38 @@
 $(document).ready(function() {
     const curTime = moment();
 
-    console.log('Start', curTime);
 
-    $('.container').append('<table style="width: 100%" class="table" id="myPlan"></table>');
-    $('.table').append('<thead><tr><th scope="col">' + "Time" + '</th><th scope="col">' + "Description" + ' </th><th scope="col">' + "Save" + ' </th></tr></thead> ');
 
-    $('.table').append('<tbody></tbody>');
+    console.log('Loaded');
 
-    for (var i = 0; i < 24; i++) {
-        console.log(i);
-        var tableRow = 'block' + i;
-        console.log(tableRow);
+    // $('.container').append('<table style="width: 100%" class="table" id="myPlan"></table>');
+    // $('.table').append('<thead><tr><th scope="col">' + "Time" + '</th><th scope="col">' + "Description" + ' </th><th scope="col">' + "Save" + ' </th></tr></thead> ');
 
-        $('.table tbody').append('<tr><td class="time-block">' + i + '</td><td class="description"></td><td class="saveBtn"><span style="font-size: 40px"><i class="far fa-calendar-check"></i></span></td></tr>');
-    }
+    // $('.table').append('<tbody></tbody>');
+
+    // for (var i = 0; i < 24; i++) {
+    //     console.log(i);
+    //     var tableRow = 'block' + i;
+    //     console.log(tableRow);
+
+    //     $('.table tbody').append('<tr><td class="time-block hour">' + i + '</td><td class="description"><textarea type="text" placeholder="Add New Appointment"></textarea></td><td class="saveBtn"><span style="font-size: 30px"><i class="far fa-calendar-check"></i></span></td></tr>');
+
+
+    // }
 
     function clock() {
         $("#currentDay").html(moment().format('dddd, MMMM Do YYYY, h:mm:ss a'));
     }
 
     setInterval(clock, 1000);
+
+
+    $('.saveBtn').on('click', function() {
+        var input = $('.description');
+        var obj = JSON.stringify(input)
+        localStorage.setItem('description', obj);
+        console.log("localStorage", localStorage)
+    })
+
 
 });
