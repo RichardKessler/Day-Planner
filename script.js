e8455da0131797b3630b3b1f7e67bc6bfbab2a40
JidMenosta <jidmenosta@gmail.com>
2020-03-21 13:23:32 -0400

    added color scheme to update dynamically to current hour

--- script.js
+++ script.js
@@ -1,39 +1,68 @@
 //Create time element
+var hour;
+var input;
 $(document).ready(function() {
+
     const curTime = moment();
 
-
-
     console.log('Loaded');
-
-    // $('.container').append('<table style="width: 100%" class="table" id="myPlan"></table>');
-    // $('.table').append('<thead><tr><th scope="col">' + "Time" + '</th><th scope="col">' + "Description" + ' </th><th scope="col">' + "Save" + ' </th></tr></thead> ');
-
-    // $('.table').append('<tbody></tbody>');
-
-    // for (var i = 0; i < 24; i++) {
-    //     console.log(i);
-    //     var tableRow = 'block' + i;
-    //     console.log(tableRow);
-
-    //     $('.table tbody').append('<tr><td class="time-block hour">' + i + '</td><td class="description"><textarea type="text" placeholder="Add New Appointment"></textarea></td><td class="saveBtn"><span style="font-size: 30px"><i class="far fa-calendar-check"></i></span></td></tr>');
-
-
-    // }
 
     function clock() {
         $("#currentDay").html(moment().format('dddd, MMMM Do YYYY, h:mm:ss a'));
     }
 
     setInterval(clock, 1000);
-
-
-    $('.saveBtn').on('click', function() {
-        var input = $('.description');
-        var obj = JSON.stringify(input)
-        localStorage.setItem('description', obj);
-        console.log("localStorage", localStorage)
-    })
-
-
+    changeColor();
+    saveInput();
 });
+
+
+function changeColor() {
+    let realTime = moment().hours();
+    console.log("Current Time" + realTime);
+
+    $(".details").each(function() {
+        var timeTest = parseInt($(this).attr("id"));
+        console.log(timeTest);
+
+        if (realTime > timeTest) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (realTime < timeTest) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+}
+
+$('.saveBtn').on('click', function() {
+    input = $(this).siblings('.details').val();
+    console.log("this", this)
+    console.log("input", input)
+
+
+    hour = $(this).siblings('.hour').text();
+    console.log("hour", hour)
+
+    localStorage.setItem(hour, JSON.stringify(input));
+
+    changeColor();
+    saveInput();
+
+})
+
+function saveInput() {
+    var slot1 = JSON.parse(localStorage.getItem("6:00 AM"));
+    $('#6').val("");
+    $('#6').val(slot1);
+
+    var slot2 = JSON.parse(localStorage.getItem("7:00 AM"));
+    $('#7').val("");
+    $('#7').val(slot2);
+}
