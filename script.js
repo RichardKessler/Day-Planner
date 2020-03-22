ab0e7b2fdc532b71ea04a2a282ea92f4fc7ec6b2
JidMenosta <jidmenosta@gmail.com>
2020-03-21 22:45:06 -0400

    Made an array for the local storage data to save and reference when data is saved and the page is loaded

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Create time element
 var hour;
-var input;
+var myInput;
 $(document).ready(function() {
 
     const curTime = moment();
@@ -42,27 +42,32 @@ function changeColor() {
 }
 
 $('.saveBtn').on('click', function() {
-    input = $(this).siblings('.details').val();
-    console.log("this", this)
-    console.log("input", input)
+    var myInput = $(this).siblings('.input').children('.details').val();
+    console.log("this", this.id)
+    console.log("input", myInput)
+    console.log($(".details"))
 
 
     hour = $(this).siblings('.hour').text();
     console.log("hour", hour)
 
-    localStorage.setItem(hour, JSON.stringify(input));
+    localStorage.setItem(hour, JSON.stringify(myInput));
 
     changeColor();
-    saveInput();
+    saveInput($(this));
 
 })
 
 function saveInput() {
-    var slot1 = JSON.parse(localStorage.getItem("6:00 AM"));
-    $('#6').val("");
-    $('#6').val(slot1);
 
-    var slot2 = JSON.parse(localStorage.getItem("7:00 AM"));
-    $('#7').val("");
-    $('#7').val(slot2);
+    var milTime = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM'];
+
+    for (var i = 0; i < milTime.length; i++) {
+
+        var slot1 = JSON.parse(localStorage.getItem(milTime[i]));
+
+        $('#' + (i + 6)).val(slot1);
+        console.log("saveInput -> i", $(i))
+
+    }
 }
